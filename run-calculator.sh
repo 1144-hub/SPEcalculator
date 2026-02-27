10f3c928227baff4f41d847e57dc145a7bcd0132
1144-hub <goyalsarthak2001@gmail.com>
2026-02-28 01:51:50 +0530

    ansible playbook

---
+++ run-calculator.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "======================================"
+echo " Launching Scientific Calculator..."
+echo "======================================"
+# We use -it so the user can interact with the menu
+docker run -it goyalsarthak/scientific-calculator:v1
